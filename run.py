777750f21a7be4a7d1c2583b376e06ca7301dfca
Janine Flores <janinerienee@gmail.com>
2022-07-08 01:38:39 +0800

    commit

--- run.py
+++ run.py
@@ -18,4 +18,4 @@ if __name__ == '__main__':
     #     )""")
     # conn.commit()
     # conn.close()
-    app.run(debug=True, host='0.0.0.0', port='8888')
+    app.run(debug=True, host='0.0.0.0', port='9999')
